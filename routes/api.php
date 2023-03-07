703d07b354729a66bed8295d2c55ff4601b0542b
a.emam <abe@crossworkers.com>
2023-03-07 19:04:35 +0200

    Add tokens scopes for Autherization

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,18 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::group(['prefix' => 'user'], function(){
+Route::group(['prefix' => 'user'], function () {
     Route::post('/login', [UserController::class, 'login'])->name('login');
     Route::post('/register', [UserController::class, 'register'])->name('register');
 
-    Route::group(['middleware' => 'auth:api'], function(){
+    Route::group(['middleware' => ['auth:api', 'scope:user']], function () {
     });
 });
 
-Route::group(['prefix' => 'admin'], function(){
+Route::group(['prefix' => 'admin'], function () {
     Route::post('/login', [AdminController::class, 'login'])->name('admin-login');
     Route::post('/register', [AdminController::class, 'register'])->name('admin-register');
 
-    Route::group(['middleware' => 'auth:admin'], function(){
+    Route::group(['middleware' => ['auth:admin', 'scope:admin']], function () {
     });
 });
